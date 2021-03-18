b3ab3ef4318a2e04b211ce0109771797640904a8
Niseme <71128250+Niseme@users.noreply.github.com>
2021-03-18 10:30:13 +0200

    Nested Components, React Tools

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { createElement } from 'react';
 import ReactDom from 'react-dom';
 
 
 function Greeting() {
-  return <h4>Hello world</h4>;
+  return (
+    <div>
+      <Person />
+      <Message />
+
+    </div>
+  );
 }
+
+const Person = () => <h2>John does</h2>
+const Message = () => { return (<p>this is my message</p>) }
 
 ReactDom.render(<Greeting />, document.getElementById('root'));
 
